61132a1da5b61bd8f41e47b7d76553c49a698ae7
mulkilutfhi <56911844+mulkilutfhi@users.noreply.github.com>
2020-09-20 16:20:38 +0700

    fixing

--- cypress/integration/examples/efishery/FreshEfishery.js
+++ cypress/integration/examples/efishery/FreshEfishery.js
@@ -7,6 +7,7 @@ describe ('test efishery',()=>{
         //memilih kota jakarta
         cy
           .get('span[class="f-16 f-bold"]').click()
+          .wait(3000)
         cy
           .get('.option-item')
           .find('#Jakarta').click({force:true})
@@ -14,24 +15,30 @@ describe ('test efishery',()=>{
         //memilih kota bandung
         cy
           .get('span[class="f-16 f-bold"]').click()
+          .wait(3000)
         cy
           .get('.option-item')
           .find('#Bandung').click({force:true})
+          
 
         //memastikan ada banner  
         cy
+          .wait(5000)
           .get('img[alt="Payday Promo"]').eq(0)
           .should('be.visible')
         cy
+          .wait(3000)
+          .get('.carousel-control-next-icon').click()
           .get('img[alt="Dari Pembudidaya untuk Anda"]')
           .should('be.visible')
         cy
+          .wait(3000)
+          .get('.carousel-control-next-icon').click()
           .get('img[alt="Payday Promo"]').eq(2)
           .should('be.visible')
 
         //memastikan catalog option
         cy
-          .get('.catalogue-card').should('have.length',4)
           .get('#eFisheryFresh').should('be.visible')
           .get('#Blibli').should('be.visible')
           .get('#Tokopedia').should('be.visible')
